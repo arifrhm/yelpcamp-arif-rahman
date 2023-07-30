5e154e3e564ab02c2534c7e3c1197738836d6539
Arif Rahman <arif.rahman.rhm@gmail.com>
2023-07-30 18:53:47 +0700

    Fix : Deployment

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const reviewRoutes = require('./routes/reviews');
 const MongoDBStore = require("connect-mongo")(session);
 
 const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
-
+console.log(dbUrl);
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
